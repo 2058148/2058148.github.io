a89fb74be82b82d0062b93d3126bb6e2ac3297b2
2058148 <87869754+2058148@users.noreply.github.com>
2021-07-24 03:08:51 +0900

    final report

---
+++ sample.js
@@ -0,0 +1,112 @@
+var count=0;
+var point6;
+function answer(point)
+{
+
+    if(point==1)
+    {
+        alert("大正解！！\n20point獲得！");
+        count++;
+    }
+    else if(point==2)
+    {
+        alert("東京怖い、、");
+    }
+    else if(point==3)
+    {
+        alert("ほっかいどーはでっかいどー");
+    }
+    else if(point==4)
+    {
+        alert("生まれたかったなあ");
+    }
+
+}
+
+function answer2(point2)
+{
+
+    if(point2==1)
+    {
+        alert("それはエイプリルフール");
+    }
+    else if(point2==2)
+    {
+        alert("夏は苦手")
+    }
+    else if(point2==3)
+    {
+       alert("誕生日プレゼントとクリスマスプレゼントが一緒ってマジ！？！？");
+    }
+    else if(point2==4)
+    {
+        alert("大正解！！\n20point獲得！");
+        count++;
+    }
+}
+
+function answer3(point3)
+{
+    
+    if(point3==1)
+    {
+        alert("ブンブンハロユーチューブ");
+    }
+    else if(point3==2)
+    {
+        alert("かっ飛ばせ～こーばやしー");
+    }
+    else if(point3==3)
+    {
+        alert("大正解！！\n20point獲得！！");
+        count++;
+    }
+    else if(point3==4)
+    {
+        alert("25m長すぎ、、");
+    }
+}
+
+function answer4(point4)
+{
+    
+    if(point4==1)
+    {
+        alert("レア度高い割に知り合いに多い");
+    }
+    else if(point4==2)
+    {
+        alert("おいしいなあ");
+    }
+    else if(point4==3)
+    {
+        alert("大正解！！\n20point獲得！！");
+        count++;
+    }
+    else if(point4==4)
+    {
+        alert("僕は嫌だ！！♪♪");
+    }
+
+}
+function answer5(point5)
+{
+    
+    if(point5==1)
+    {
+        alert("手と足が一緒になっちゃう、、");
+    }
+    else if(point5==2)
+    {
+        alert("大正解！！\n20point獲得！！");
+        count++;
+    }
+    else if(point5==3)
+    {
+        alert("ハマ・オカモトさんが好きです。");
+    }
+    else if(point5==4)
+    {
+        alert("弾けたらなあ");
+    }
+}
